4335491fb3ce71ed7702df73e55392a26b9ed2b7
malcolm <malcolm@hoprocker.net>
2014-01-04 14:58:44 -0800

    update bootstrap settings

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,9 @@
 ## virtualenv must be installed
 ##
 
+apt-get install -y python2.7 python2.7-dev redis-server python-pip libxml2-dev libxslt-dev libssl-dev lib32z1-dev
+## change 'lib32z1-dev' to 'zlib1g-dev' for 32-bit
+pip install virtualenv
 virtualenv --no-site-packages ./env
 ./env/bin/pip install --upgrade -r ./requirements.txt
 
