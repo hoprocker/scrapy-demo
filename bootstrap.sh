0612c416606700685b73bd7498beb69b2c7df94a
malcolm <malcolm@hoprocker.net>
2014-01-04 20:46:56 -0800

    get the deploy working

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 ##
 ## setup script for this repo
-## virtualenv must be installed
 ##
 
-apt-get install -y python2.7 python2.7-dev redis-server python-pip libxml2-dev libxslt-dev libssl-dev lib32z1-dev
-## change 'lib32z1-dev' to 'zlib1g-dev' for 32-bit
+curl -s http://archive.scrapy.org/ubuntu/archive.key | apt-key add - ## get GPG key
+echo "deb http://archive.scrapy.org/ubuntu saucy main" > /etc/apt/sources.list.d/scrapy.list
+apt-get update
+apt-get install -y python2.7 python2.7-dev redis-server python-pip libxml2-dev libxslt-dev libssl-dev scrapyd lib32z1-dev
+## change 'lib32z1-dev' above to 'zlib1g-dev' for 32-bit
 pip install virtualenv
 virtualenv --no-site-packages ./env
 ./env/bin/pip install --upgrade -r ./requirements.txt
-
+chown -R ubuntu /www
+cp ./scrapyd.conf /etc/scrapyd/scrapyd.conf
+curl http://localhost:6800/delproject.json -d project=realtortest
+/bin/bash ./env/bin/activate
+pushd realtortest && ../env/bin/scrapyd-deploy scrapyd -p realtortest && popd
--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,13 @@ USER=ubuntu
 SERVER=54.200.177.70
 ROOT="/www"   ## no trailing slash
 
+echo "deploying code"
+echo ""
+ssh $USER@$SERVER "sudo mkdir -p $ROOT && sudo chown -R $USER $ROOT"
 rsync -axv --delete --ignore-errors --exclude ".git" --exclude "env" ~/dev/scrapy-test/ $USER@$SERVER:$ROOT/
-ssh $USER@$SERVER "pushd $ROOT && . ./env/bin/activate && sudo /bin/bash ./bootstrap.sh"
-ssh $USER@$SERVER "pushd $ROOT && $ROOT/env/bin/python $ROOT/main.py"
+
+echo "killing old processes"
+echo ""
+ssh $USER@$SERVER "sudo /bin/netstat -alpn | grep 9097 | awk '{print $7}' | grep python | sed -e 's/\/python//' | sudo xargs kill"
+ssh $USER@$SERVER "pushd $ROOT && sudo /bin/bash ./bootstrap.sh"
+ssh $USER@$SERVER "pushd $ROOT && $ROOT/env/bin/python $ROOT/main.py &"
