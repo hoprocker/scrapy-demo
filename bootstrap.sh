0938cf64147d71d8b48c6900ae56ebabf1187ed5
malcolm <malcolm@hoprocker.net>
2014-01-04 21:48:02 -0800

    deployment tweaks

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,11 +8,8 @@ echo "deb http://archive.scrapy.org/ubuntu saucy main" > /etc/apt/sources.list.d
 apt-get update
 apt-get install -y python2.7 python2.7-dev redis-server python-pip libxml2-dev libxslt-dev libssl-dev scrapyd lib32z1-dev
 ## change 'lib32z1-dev' above to 'zlib1g-dev' for 32-bit
-pip install virtualenv
-virtualenv --no-site-packages ./env
-./env/bin/pip install --upgrade -r ./requirements.txt
-chown -R ubuntu /www
+pip install --upgrade -r ./requirements.txt
 cp ./scrapyd.conf /etc/scrapyd/scrapyd.conf
 curl http://localhost:6800/delproject.json -d project=realtortest
-/bin/bash ./env/bin/activate
-pushd realtortest && ../env/bin/scrapyd-deploy scrapyd -p realtortest && popd
+/bin/bash activate
+pushd realtortest && scrapyd-deploy scrapyd -p realtortest && popd
