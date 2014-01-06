47c40c6b3f2d103adf38f422e7be7d7c8756072a
malcolm <malcolm@hoprocker.net>
2014-01-05 16:34:50 -0800

    adding item loader parsing

--- realtortest/realtortest/items.py
+++ realtortest/realtortest/items.py
@@ -4,6 +4,12 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 from scrapy.item import Item, Field
+from scrapy.contrib.loader import ItemLoader
+from scrapy.contrib.loader.processor import TakeFirst, Compose
+from types import ListType, TupleType
+import re
+
+DEBUG = False
 
 class ListingItem(Item):
     street_address = Field()
@@ -15,3 +21,58 @@ class ListingItem(Item):
     baths = Field()
     sqft = Field()
     property_type = Field()
+
+### helpers for the loader
+def strip_extra(val):
+    """
+    strip whitespace and the outer <span> elements
+    """
+    if type(val) in [ListType, TupleType] and len(val) > 0:
+        val = val[0]
+    if isinstance(val, basestring):
+        val = val.strip()
+        srch_val = re.search("<span[^>]+>(.*)<\/span>", val)
+        if srch_val:
+            return srch_val.groups()[0]
+        return val
+    return val
+def parse_num_fld(val):
+    if DEBUG:
+        print "VAL: %s" % val
+    if type(val) in [ListType, TupleType] and len(val) > 0:
+        val = val[0]
+    if isinstance(val, basestring):
+        try:
+            return float(re.sub("[^0-9.]+", "", val))
+        except ValueError:
+            pass
+    return val
+def parse_baths(val):
+    """
+    extract only 'Full' baths
+    """
+    if DEBUG:
+        print "VAL: %s" % val
+    if type(val) in [ListType, TupleType] and len(val) > 0:
+        val = val[0]
+    if isinstance(val, basestring):
+        real_val = re.search("([0-9.]+)[a-z><\/]+ Full", val)
+        if real_val:
+            if DEBUG:
+                print "real_val: %s" % (real_val.groups(),)
+            return float(real_val.groups()[0])
+    return val
+
+def print_output(val):
+    if DEBUG:
+        print "output val: %s" % val
+    return val
+        
+class ListingItemLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+    default_input_processor = Compose(strip_extra, print_output)
+
+    sqft_in = Compose(strip_extra, parse_num_fld, print_output)
+    beds_in = Compose(strip_extra, parse_num_fld, print_output)
+    price_in = Compose(strip_extra, parse_num_fld, print_output)
+    baths_in = Compose(strip_extra, parse_baths, print_output)
--- realtortest/realtortest/spiders/listing_scraper.py
+++ realtortest/realtortest/spiders/listing_scraper.py
@@ -3,34 +3,33 @@ from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy import log
 from scrapy.http import Request
 from scrapy.selector import Selector
-from realtortest.items import ListingItem
+from realtortest.items import ListingItem, ListingItemLoader
 from realtortest.extractor import PagerExtractor
 import re
 
 class ListingSpider(CrawlSpider):
     name = "realtor"
-    start_urls = ["http://www.realtor.com/realestateandhomes-search/Sonora_CA",]
+    #start_urls = ["http://www.realtor.com/realestateandhomes-search/Sonora_CA",]
+    start_urls = ["file:///Users/malcolm/Downloads/realtor.com.html"]
 
     rules = (Rule(link_extractor=PagerExtractor(), callback="parse_page"),)
 
     def parse_page(self, resp):
-        base_http = re.search("^(http[s]?://[^\/]*)/.*", resp.url).groups()
+        base_http = re.search("^([a-z]+://[\/]?[^\/]*)/", resp.url).groups()
         if base_http:
             base_http = base_http[0]
         log.msg("response recieved from %s" % (resp.url))
         
         sel = Selector(resp)
-        for listing_sel in sel.xpath("//*[@class='listing-summary-wrapper group']"):
+        for listing_sel in sel.xpath("//*[contains(@class,'listing-summary-wrapper group')]"):
             item = ListingItem()
+            loader = ListingItemLoader(item=item, selector=listing_sel)
             for f in item.fields:
                 xpath_sel = "listing-%s" % (f.replace("_", "-"))
-                fld = listing_sel.xpath("ul//*[@class='%s']/text()" % xpath_sel)
-                if len(fld) > 0:
-                    val = fld[0].extract().strip()
-                    log.msg("item: %s --> %s" % (xpath_sel, val))
-                    item[f] = val
-            yield item
-
+                loader.add_xpath(f, "ul//*[contains(@class, '%s')]" % xpath_sel)
+            yield loader.load_item()  ## return items
+        '''
         for link in sel.xpath("//ol[@class='pagination pagination-pos-b']/li/a[@class='paginate ']/@href").extract():
             log.msg("returning %s%s to follow" % (base_http,link))
             yield Request("%s%s" % (base_http,link))
+        '''
