02a364de299f3988425a6870d1112beebc3d2544
malcolm <malcolm@hoprocker.net>
2014-01-05 23:13:50 -0800

    fix deployment

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,6 @@
 ## setup script for this repo
 ##
 
-curl -s http://archive.scrapy.org/ubuntu/archive.key | apt-key add - ## get GPG key
-echo "deb http://archive.scrapy.org/ubuntu saucy main" > /etc/apt/sources.list.d/scrapy.list
-apt-get update
 apt-get install -y python2.7 python2.7-dev redis-server python-pip libxml2-dev libxslt-dev libssl-dev scrapyd lib32z1-dev
 ## change 'lib32z1-dev' above to 'zlib1g-dev' for 32-bit
 pip install --upgrade -r ./requirements.txt
--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ rsync -axv --delete --ignore-errors --exclude ".git" --exclude "env" ~/dev/scrap
 
 echo "killing old processes"
 echo ""
-ssh $USER@$SERVER "sudo /bin/netstat -alpn | grep 9097 | awk '{print $7}' | grep python | sed -e 's/\/python//' | sudo xargs kill"
+ssh $USER@$SERVER "sudo /bin/netstat -alpn | grep 9097 | awk '{print \$7}' | grep python | sed -e 's/\/python//' | sudo xargs kill"
 ssh $USER@$SERVER "pushd $ROOT && sudo /bin/bash ./bootstrap.sh"
 ssh $USER@$SERVER "sudo chown -R $USER $ROOT"
 ssh $USER@$SERVER "pushd $ROOT && $ROOT/env/bin/python $ROOT/main.py &"
